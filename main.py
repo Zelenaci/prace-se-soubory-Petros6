4b2919edb41a71d0f5d2e8754e13faa049873001
Petros <skv38169@spseol.cz>
2021-11-15 09:25:11 +0100

    menu

--- main.py
+++ main.py
@@ -5,13 +5,34 @@
 # Autor:
 ############################################################################
 from random import randint, choice
-
+from os.path improt exist
 ############################################################################
 
 
-def f():
-    return None
+def a():
+    vstup = input('soubor na vstupu> ')
+    i exists(vstup):
+        pass
+    print('A')
+
+def b():
+    print('B')
+if __name__=='__main__':
+    print("""
+co chceš dělat?
+1) převod souboru na malá písmena
+2) nahrazení znaků
+
+    while true:
+            volba = input('zadej svou volbu >')
+            if volba.isdigit():
+                volba = int(volba)
+                break
+if volba ==1:
+    a()
+elif volba ==2:
+    b()
+else:
+     print('nic')
 
 
-if __name__ == "__main__":
-    print(f())
